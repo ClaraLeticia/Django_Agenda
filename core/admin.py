67a2d704a963333f2b332624bfc967c358749253
Clara <claralet1346@gmail.com>
2024-05-06 14:16:54 -0300

    Criando o model evento

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import evento
 
 # Register your models here.
+
+class evento_admin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento')
+
+admin.site.register(evento, evento_admin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(null=True, blank=True)
+    data_evento = models.DateTimeField()
+    data_criacao = models.DateTimeField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
